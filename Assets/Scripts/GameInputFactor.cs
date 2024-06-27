4cb8e9e166bec4d0204fc9a8cd411346229b1117
ChulumancoMbete <chulumancombete50@gmail.com>
2024-06-27 15:37:50 +0200

    Interact Action, C# Events

--- Assets/Scripts/GameInputFactor.cs
+++ Assets/Scripts/GameInputFactor.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInputFactor : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
+
     private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
         playerInputActions =  new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(InputAction.CallbackContext obj)
+    {
+        /*if (OnInteractAction != null)
+        {
+            OnInteractAction(this, EventArgs.Empty);
+        }*/
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
