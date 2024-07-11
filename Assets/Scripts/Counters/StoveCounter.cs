0e3b99cffe0c237edc8b3677d1db3b2768ef0fd6
ChulumancoMbete <chulumancombete50@gmail.com>
2024-07-11 16:21:41 +0200

    Plate Complete Visual

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static CuttingCounter;
 
 public class StoveCounter : BaseContainer, IHasProgress
 {
