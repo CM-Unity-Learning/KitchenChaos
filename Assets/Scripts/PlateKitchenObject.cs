8d4915fec880f1dd7eb91e1a6df9faea43d6f79f
ChulumancoMbete <chulumancombete50@gmail.com>
2024-07-15 10:56:42 +0200

    Plate World UI Icons

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -38,8 +38,11 @@ public class PlateKitchenObject : KitchenObject
             });
 
             return true;
-        }
-        
+        } 
     }
 
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 }
